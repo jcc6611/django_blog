427f80316176777ef0329eabf719b08041c4162f
jcc6611 <jcc6611@rit.edu>
2020-06-15 00:05:12 -0400

    added login/register links to navbar. added account page.

--- container/initials/settings.py
+++ container/initials/settings.py
@@ -118,6 +118,10 @@ STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
