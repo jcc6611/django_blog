13b76c0a54aa0fef9879e833edba57b0d46427be
jcc6611 <jcc6611@rit.edu>
2020-06-22 18:20:32 -0400

    added password reset

--- container/initials/settings.py
+++ container/initials/settings.py
@@ -125,6 +125,13 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
